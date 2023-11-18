f2558958e318fce12c3c710b99c1f8b84c439f6e
NB <altmshfkgudtjr@naver.com>
2023-11-19 01:25:23 +0900

    [chore] API Endpoint 업데이트 & 이력 변경 (#34)

--- src/controllers/common.js
+++ src/controllers/common.js
@@ -1,19 +1,19 @@
 import Fetch from 'controllers/fetch'
 
 export const getCount = () => {
-	return Fetch('https://api.countapi.xyz/hit/altmshfkgudtjr.github.io/counter', "GET").then(res => {
+	return Fetch('https://api.counterapi.dev/v1/altmshfkgudtjr.github.io/counter/up', "GET").then(res => {
 		return res;
 	});
 }
 
 export const getLike = () => {
-	return Fetch('https://api.countapi.xyz/info/altmshfkgudtjr.github.io/profileLike', "GET").then(res => {
+	return Fetch('https://api.counterapi.dev/v1/altmshfkgudtjr.github.io/profileLike', "GET").then(res => {
 		return res;
 	});
 }
 
 export const hitLike = () => {
-	return Fetch('https://api.countapi.xyz/hit/altmshfkgudtjr.github.io/profileLike', "GET").then(res => {
+	return Fetch('https://api.counterapi.dev/v1/altmshfkgudtjr.github.io/profileLike/up', "GET").then(res => {
 		return res;
 	});
 }
